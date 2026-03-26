6a79017337b5b845161ed751b95bee22a56cc84d
BJY029 <go333dragon@gmail.com>
2026-03-26 16:15:28 +0900

    DTO settings
    통신에 사용할 DTO 설계

--- Assets/0_Scripts/AISimulation/BalanceSimulator.cs
+++ Assets/0_Scripts/AISimulation/BalanceSimulator.cs
@@ -1,13 +1,90 @@
+using System.Collections.Generic;
 using System.IO;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class SimGameState
 {
-    public int p1VillHP, p1VillBarrier, p1Energy;
-    public int p2VillHP, p2VillBarrier, p2Energy;
+    public int wave;
+    public int day;
+
+    public int p1Energy, p2Energy;
+    public float p1VillHP, p1VillBarrier;
+    public float p2VillHP, p2VillBarrier;
+    public bool p1HasDebuff, p2HasDebuff;
     public float treeHP;
     public float treeToxicDmg;
+
+    public List<string> p1Inventory = new List<string>();
+    public List<string> p2Inventory = new List<string>();
+
+    public SimGameState(PlayerSetting playerSetting, RoomSetting roomSetting)
+    {
+        this.p1Energy = this.p2Energy = playerSetting.initialEnergy;
+        this.p1VillHP = this.p2VillHP = playerSetting.villageHP;
+        this.p1VillBarrier = this.p2VillBarrier = playerSetting.villageBarrier;
+        this.treeHP = roomSetting.treeHP;
+        this.treeToxicDmg = roomSetting.treeAtkPow;
+        this.wave = roomSetting.initialWave;
+        this.day = roomSetting.startDay;
+    }
+
+    public LLMGameStateDTO GetStateForPlayer(int myPlayerNum)
+    {
+        LLMGameStateDTO dto = new LLMGameStateDTO();
+
+        dto.currentWave = this.wave;
+        dto.expectedToxicDamage = this.treeToxicDmg;
+        dto.treeHP = this.treeHP;
+
+        if (myPlayerNum == 1)
+        {
+            dto.myStatus = CreatePlayerDTO(p1VillHP, p1VillBarrier, p1Energy, p1HasDebuff);
+            dto.oppStatus = CreatePlayerDTO(p2VillHP, p2VillBarrier, p2Energy, p2HasDebuff);
+            dto.myInventory = CreateInventoryDTO(p1Inventory);
+            dto.oppInventory = CreateInventoryDTO(p2Inventory);
+        }
+        else
+        {
+            dto.myStatus = CreatePlayerDTO(p2VillHP, p2VillBarrier, p2Energy, p2HasDebuff);
+            dto.oppStatus = CreatePlayerDTO(p1VillHP, p1VillBarrier, p1Energy, p1HasDebuff);
+            dto.myInventory = CreateInventoryDTO(p2Inventory);
+            dto.oppInventory = CreateInventoryDTO(p1Inventory);
+        }
+        return dto;
+    }
+
+    private PlayerStatusDTO CreatePlayerDTO(float hp, float barrier, int energy, bool Debuff)
+    {
+        return new PlayerStatusDTO
+        {
+            villageHP = hp,
+            barrier = barrier,
+            energy = energy,
+            hasDebuff = Debuff
+        };
+    }
+
+    private List<ItemInfoDTO> CreateInventoryDTO(List<string> inventoryIds)
+    {
+        List<ItemInfoDTO> invList = new List<ItemInfoDTO>();
+        foreach (var id in inventoryIds)
+        {
+            ItemSO itemSo = ItemDB.Instance.Get(id);
+            if (itemSo != null)
+            {
+                invList.Add(new ItemInfoDTO
+                {
+                    itemId = itemSo.itemId,
+                    name = itemSo.displayName_ID,
+                    cost = itemSo.itemCost,
+                    rarity = itemSo.itemClass,
+                    type = itemSo.type
+                });
+            }
+        }
+        return invList;
+    }
 
 }
 
@@ -32,7 +109,17 @@ public class BalanceSimulator : MonoBehaviour
     {
         for (int cnt = 1; cnt <= gameCount; cnt++)
         {
+            SimGameState state = new SimGameState(playerSetting, roomSetting);
+            bool isGameOver = false;
+            int winner = 0;
+            int turnCount = 0;
 
+            while (!isGameOver && turnCount < 50)
+            {
+                turnCount++;
+
+
+            }
         }
     }
 }
--- Assets/0_Scripts/AISimulation/DTO/LLMDataTypes.cs
+++ Assets/0_Scripts/AISimulation/DTO/LLMDataTypes.cs
@@ -1,6 +1,10 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 
 //요청 클래스
+[System.Serializable]
 public class LLMRequest
 {
     //사용 모델명: Gemma3:12b
@@ -9,16 +13,55 @@ public class LLMRequest
     public ResponseFormat response_format = new ResponseFormat { type = "json_object" };
 }
 
+[System.Serializable]
 public class Message { public string role; public string content; }
 
+[System.Serializable]
 public class ResponseFormat { public string type; }
 
+[System.Serializable]
+public class LLMGameStateDTO
+{
+    public int currentWave;
+    public float expectedToxicDamage;
+    public float treeHP;
+
+    public PlayerStatusDTO myStatus;
+    public PlayerStatusDTO oppStatus;
+    public List<ItemInfoDTO> myInventory;
+    public List<ItemInfoDTO> oppInventory;
+}
+
+[System.Serializable]
+public class PlayerStatusDTO
+{
+    public float villageHP;
+    public float barrier;
+    public int energy;
+    public bool hasDebuff;
+}
+
+[System.Serializable]
+public class ItemInfoDTO
+{
+    public string itemId;
+    public string name;
+    public int cost;
+    [JsonConverter(typeof(StringEnumConverter))]
+    public ItemClass rarity;
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public ItemType type;
+}
+
+[System.Serializable]
 public class LLMDecision
 {
     public string reasoning;
     public List<LLMAction> actions;
 }
 
+[System.Serializable]
 public class LLMAction
 {
     //UseItem, HitTree 등
